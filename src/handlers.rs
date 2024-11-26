528a754b5667dc1615f45f81b165211a7580adeb
olekspickle <22867443+olekspickle@users.noreply.github.com>
2024-11-26 13:40:31 +0100

    initial booststrap

---
+++ src/handlers.rs
@@ -0,0 +1,76 @@
+use rocket::{catch, get, Request};
+
+use rocket_dyn_templates::{context, Template};
+
+fn styles() -> String {
+    String::from_utf8_lossy(include_bytes!("../static/main.css")).to_string()
+}
+
+#[get("/")]
+pub fn index() -> Template {
+    Template::render(
+        "base",
+        context! {
+            styles: styles(),
+            title: "intro",
+        },
+    )
+}
+
+#[get("/")]
+pub fn first() -> Template {
+    Template::render(
+        "first",
+        context! {
+            styles: styles(),
+            title: "first",
+        },
+    )
+}
+
+#[get("/")]
+pub fn second() -> Template {
+    Template::render(
+        "second",
+        context! {
+            styles: styles(),
+            title: "second",
+        },
+    )
+}
+
+#[get("/")]
+pub fn third() -> Template {
+    Template::render(
+        "third",
+        context! {
+            styles: styles(),
+            title: "third",
+        },
+    )
+}
+
+#[catch(404)]
+pub fn not_found(req: &Request<'_>) -> Template {
+    Template::render(
+        "error/404",
+        context! {
+            styles: styles(),
+            title: "Oops",
+            uri: req.uri()
+        },
+    )
+}
+
+// Also doable, but not really beneficial
+// pub fn customize(tera: &mut Tera) {
+//     tera.add_raw_template("about.html", r#"
+//         {% extends "tera/base" %}
+
+//         {% block content %}
+//             <section id="about">
+//               <h1>About - Here's another page!</h1>
+//             </section>
+//         {% endblock content %}
+//     "#).expect("valid Tera template");
+// }
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use rocket::{
+    build, catchers,
+    fs::{relative, FileServer},
+    launch, routes,
+};
+use rocket_dyn_templates::Template;
+
+mod handlers;
+
+#[cfg(test)]
+mod tests;
+
+#[launch]
+fn rocket() -> _ {
+    build()
+        .mount("/", routes![handlers::index])
+        .mount("/first", routes![handlers::first])
+        .mount("/second", routes![handlers::second])
+        .mount("/third", routes![handlers::third])
+        .mount("/", FileServer::from(relative!("static/assets")))
+        .register("/", catchers![handlers::not_found])
+        .attach(Template::fairing())
+}
